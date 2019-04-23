fcca3693f020af0daccbc695a1c2ee3e1b8bc69d
LyallInsanity <addyvv@hotmail.nl>
2019-04-23 15:36:51 +0200

    Database connectie met een test query

--- src/magazijnrobot/DbConn.java
+++ src/magazijnrobot/DbConn.java
@@ -2,21 +2,42 @@ package magazijnrobot;
 
 import java.sql.*;
 
-class DbConn{
-public static void main(String args[]){
-        try{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wideworldimporters","root","");
+class DbConn {
+        private static Connection con;
 
-        // Test om te kijken of connectie werkt
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from orders");
-        while(rs.next())
-        System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
-        con.close();
-
-
-        }catch(Exception e){ System.out.println(e);}
+        public static void dbConnect() {
+                try {
+                        Class.forName("com.mysql.cj.jdbc.Driver");
+                        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wideworldimporters", "root", "");
+                } catch (Exception e) {
+                        System.out.println(e);
+                }
         }
+
+        public static void dbKill() {
+                try {
+                        con.close();
+                } catch (Exception e) {
+                        System.out.println(e);
+                }
         }
+
+        // Testen of de database connectie werkt
+        public static void main(String args[]) {
+                DbConn.dbConnect();
+                try {
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery("select * from orders");
+                        while (rs.next())
+                                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4));
+                        stmt.close();
+                        DbConn.dbKill();
+                } catch (Exception e) {
+                        System.out.println(e);
+                }
+
+
+        }
+}
+
 
